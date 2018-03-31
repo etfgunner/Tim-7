f954e029c8d57e38cb497e514ee64bd081a4ad03
Milan <milan.zuza18@gmail.com>
2018-03-31 14:48:17 +0200

    Added Eureka server and service discovery for Orders microservice

--- Orders/src/main/java/nwt/orders/OrdersApplication.java
+++ Orders/src/main/java/nwt/orders/OrdersApplication.java
@@ -3,11 +3,13 @@ package nwt.orders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EntityScan("nwt.orders.model")
 @EnableJpaRepositories(basePackages= "nwt.orders.repository")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrdersApplication {
 
 	public static void main(String[] args) {

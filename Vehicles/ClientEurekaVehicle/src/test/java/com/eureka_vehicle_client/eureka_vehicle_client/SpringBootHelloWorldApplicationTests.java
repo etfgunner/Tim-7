8d3f10a205caf45a78223abb3b6bbe26b7cd5c78
AyLLa93 <a.ylla@live.com>
2018-04-03 15:12:33 +0200

    Eureka klijent i server

---
+++ Vehicles/ClientEurekaVehicle/src/test/java/com/eureka_vehicle_client/eureka_vehicle_client/SpringBootHelloWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eureka_vehicle_client.eureka_vehicle_client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootHelloWorldApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

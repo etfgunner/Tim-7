8bb072c39641ef75f4e2557b02bd01bd0e6dc931
begicf <fuad.begic@gmail.com>
2018-04-20 16:07:37 +0200

    Konekciju na Eureka server - zajedničku

--- Accounts/account_m/src/main/java/com/example/account_m/Runner.java
+++ Accounts/account_m/src/main/java/com/example/account_m/Runner.java
@@ -3,9 +3,15 @@ package com.example.account_m;
 import com.example.account_m.entity.Client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.util.Arrays;
+
+
+//@EnableDiscoveryClient
 @SpringBootApplication
-public class Runner {
+public class Runner 
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Runner.class, args);

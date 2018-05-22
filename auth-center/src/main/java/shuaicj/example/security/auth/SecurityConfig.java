879c5ee16829c665c8faad953a1368f4c7c9122f
begicf <fuad.begic@gmail.com>
2018-05-23 00:05:52 +0200

    auth-center

--- auth-center/src/main/java/shuaicj/example/security/auth/SecurityConfig.java
+++ auth-center/src/main/java/shuaicj/example/security/auth/SecurityConfig.java
@@ -1,30 +1,38 @@
-package com.example.zuul;
+package shuaicj.example.security.auth;
 
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import common.JwtAuthenticationConfig;
-import common.JwtTokenAuthenticationFilter;
+import common.JwtUsernamePasswordAuthenticationFilter;
 
 /**
- * Config role-based auth.
+ * Config login authentication.
  *
  * 
  */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private JwtAuthenticationConfig config;
+    @Autowired JwtAuthenticationConfig config;
 
     @Bean
     public JwtAuthenticationConfig jwtConfig() {
         return new JwtAuthenticationConfig();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ADMIN", "USER").and()
+                .withUser("shuaicj").password("shuaicj").roles("USER");
     }
 
     @Override
@@ -40,13 +48,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .exceptionHandling().authenticationEntryPoint(
                             (req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
                 .and()
-                    .addFilterAfter(new JwtTokenAuthenticationFilter(config),
+                    .addFilterAfter(new JwtUsernamePasswordAuthenticationFilter(config, authenticationManager()),
                             UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
                     .antMatchers(config.getUrl()).permitAll()
-                    .antMatchers("/backend/admin").hasRole("ADMIN")
-                    .antMatchers("/backend/user").hasRole("USER")
-                    .antMatchers("/backend/guest").permitAll();
+                    .anyRequest().authenticated();
     }
 }
 

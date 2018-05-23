e550f29182616dd075e0c2549483256674a6b912
Fuad Begic <begicf@Mac-mini.local>
2018-05-23 08:34:07 +0200

    bug fix

--- auth-center/src/main/java/common/JwtAuthenticationConfig.java
+++ auth-center/src/main/java/common/JwtAuthenticationConfig.java
@@ -11,18 +11,18 @@ import org.springframework.beans.factory.annotation.Value;
 @ToString
 public class JwtAuthenticationConfig {
 
-    @Value("${shuaicj.security.jwt.url:/login}")
+    @Value("${security.jwt.url:/login}")
     private String url;
 
-    @Value("${shuaicj.security.jwt.header:Authorization}")
+    @Value("${security.jwt.header:Authorization}")
     private String header;
 
-    @Value("${shuaicj.security.jwt.prefix:Bearer}")
+    @Value("${security.jwt.prefix:Bearer}")
     private String prefix;
 
-    @Value("${shuaicj.security.jwt.expiration:#{24*60*60}}")
+    @Value("${security.jwt.expiration:#{24*60*60}}")
     private int expiration; // default 24 hours
 
-    @Value("${shuaicj.security.jwt.secret}")
+    @Value("${security.jwt.secret}")
     private String secret;
 }
